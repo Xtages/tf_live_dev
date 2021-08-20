2b680fafac25d019b55572c4b41150bf579c335e
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-08-20 10:52:22 -0700

    Elb and ecs perms (#7)
    * Adding ACM perms for Console
    
    * codebuild upgrade

--- codebuild/main.tf
+++ codebuild/main.tf
@@ -1,5 +1,5 @@
 module "codebuild" {
-  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.2"
+  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.3"
   env        = var.env
   account_id = var.account_id
 }
