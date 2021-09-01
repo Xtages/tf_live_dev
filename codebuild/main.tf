a9978c287eb1faaa761e9f567c5467719cc342b5
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-09-01 16:49:43 -0700

    infra changes to send logs

--- codebuild/main.tf
+++ codebuild/main.tf
@@ -1,5 +1,5 @@
 module "codebuild" {
-  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.3"
+  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.4"
   env        = var.env
   account_id = var.account_id
 }
