7de8a347d7b554e465f8341dd7cb6194db49ae3c
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-10-18 14:41:37 -0700

    Upgrade codebuild module

--- codebuild/main.tf
+++ codebuild/main.tf
@@ -1,5 +1,15 @@
+data "terraform_remote_state" "xtages_dev_vpc_infra" {
+  backend = "s3"
+  config = {
+    bucket = "xtages-dev-tfstate"
+    key    = "tfstate/us-east-1/development/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "codebuild" {
-  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.4"
+  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.7"
   env        = var.env
   account_id = var.account_id
+  subnets = data.terraform_remote_state.xtages_dev_vpc_infra.outputs.private_subnets
 }
